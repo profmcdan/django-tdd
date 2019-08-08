0a6a94697d4384bd36ca09bb87ca6c0714028254
Daniel Ale <danielale9291@gmail.com>
2019-08-08 09:49:11 +0100

    Test Model

--- birdie/models.py
+++ birdie/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,6 @@ from django.db import models
 
 class Post(models.Model):
     body = models.TextField()
+
+    def get_excerpt(self, char):
+        return self.body[:char]
--- birdie/tests/test_models.py
+++ birdie/tests/test_models.py
@@ -8,3 +8,8 @@ class TestPost:
     def test_model(self):
         obj = mixer.blend('birdie.Post')
         assert obj.pk == 1, "Should create a Post instance"
+
+    def test_get_excerpt(self):
+        obj = mixer.blend('birdie.Post', body="Hello World")
+        result = obj.get_excerpt(5)
+        assert result == "Hello", "Should return first few characters"
