3ea0b58d13a5f761dcdf89aa7abb287f3ca2b0fe
Daniel Ale <danielale9291@gmail.com>
2019-08-08 10:02:50 +0100

    Test Admin

--- birdie/admin.py
+++ birdie/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = models.Post
+    list_display = ('excerpt',)
+
+    def excerpt(self, obj):
+        return obj.get_excerpt(5)
+
+
+# Register
+admin.site.register(models.Post, PostAdmin)
---
+++ birdie/tests/test_admin.py
@@ -0,0 +1,16 @@
+import pytest
+from django.contrib.admin.sites import AdminSite
+from mixer.backend.django import mixer
+from .. import admin, models
+
+pytestmark = pytest.mark.django_db
+
+
+class TestPostAdmin:
+    def test_excerpt(self):
+        site = AdminSite()
+        post_admin = admin.PostAdmin(models.Post, site)
+        obj = mixer.blend('birdie.Post', body="Hello World")
+        result = post_admin.excerpt(obj)
+        expected = obj.get_excerpt(5)
+        assert result == expected, "Should return first few characters"
