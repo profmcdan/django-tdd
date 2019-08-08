114a7c7d7798db04e34c679b22abe4d93ebd86b3
Daniel Ale <danielale9291@gmail.com>
2019-08-08 09:41:04 +0100

    Test Setup Complete

---
+++ birdie/tests/test_models.py
@@ -0,0 +1,10 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = pytest.mark.django_db
+
+
+class TestPost:
+    def test_model(self):
+        obj = mixer.blend('birdie.Post')
+        assert obj.pk == 1, "Should create a Post instance"
